9dc950be1380bcad30303595dcad37998dec4fa2
Sanmeshkumar Udhayakumar <sanmesh1@gmail.com>
2020-12-14 20:17:18 -0500

    Got frontend rough tmplate

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,25 +1,113 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RefereeingUI extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			jsonOutputObject:null,
+			counter: 0,
+			teamOrPlayer: "Player",
+			teamOrPlayerName: "",
+			cheating: {disadvantaged: null, commiting: null, totalPercentage: null},
+			protecting: {disadvantaged: null, commiting: null, totalPercentage: null},
+		};
+		this.callApi = this.callApi.bind(this);
+		this.handleChangeTeamOrPlayerName = this.handleChangeTeamOrPlayerName.bind(this);
+		this.handleChangeTeamOrPlayer = this.handleChangeTeamOrPlayer.bind(this);
+		
+	}
+	handleChangeTeamOrPlayerName(event){
+		this.setState({teamOrPlayerName: event.target.value});
+	}
+	handleChangeTeamOrPlayer(event){
+		this.setState({teamOrPlayer: event.target.value});
+	}
+	callApi(){
+		const apiUrl = 'https://original-spider-273806.ue.r.appspot.com/get_data';
+		fetch(apiUrl)
+		.then((response) => 
+		response.json()
+		)
+		.then((data) => {
+			console.log('This is your data', data);
+			console.log(typeof data);
+			console.log(Object.getOwnPropertyNames(data));
+			console.log(data[0].gameId);
+			console.log(typeof data[0].gameId);
+			this.setState({jsonOutputObject: data,
+			counter: this.state.counter+1,
+			});
+		}
+	);
+}
+  render() {
+	return (
+		<div className="Parent">
+			<div className="RefereeingInput">
+				{/*
+				<h1> {this.state.text} </h1>
+				<h1> {this.state.teamOrPlayer} </h1>
+				<h1> {this.state.teamOrPlayerName} </h1>
+				*/}
+				<select className="teamOrPlayer" value={this.state.teamOrPlayer} onChange={this.handleChangeTeamOrPlayer}>
+					<option value="Player">Player</option>
+					<option value="Team">Team</option>
+				</select>
+				<input className="teamOrPlayerName" type="text" value={this.state.teamOrPlayerName} onChange={this.handleChangeTeamOrPlayerName} />
+				<button className="callApi" onClick= {this.callApi}> 
+					 Submit
+				</button>
+			</div>
+			<div className="RefereeingAccuracyOutput">
+					How favorable are refs?
+					
+					<br /><br />REFEREE CHEATING:
+					
+					<br /><br />Referees being blind to fouls commited by player % = 
+					commiting_INC/(commiting_CC+ commiting_INC)
+					<br />{this.state.cheating.commiting}
+					<br /><br />Referees taking out opponent by calling incorrect fouls against player opponent % = 
+					disadv_IC/(disadv_CNC+ disadv_IC)
+					<br />{this.state.cheating.disadvantaged}
+
+					<br /><br />Total Cheating %: 
+					(commiting_INC+disadv_IC)/(commiting_CC+ commiting_INC + disadv_CNC+ disadv_IC)
+					<br />{this.state.cheating.totalPercentage}
+
+					<br /><br />Comparison to avg cheating %:
+
+					<br /><br />Show everything in bar chart and box chart
+
+					<br /><br />PROTECTING PLAYER CORRECTLY:
+
+					<br /><br />Referees not calling fouls on player unnecessarily % = commiting_CNC/(commiting_IC+ commiting_CNC)
+					<br />{this.state.protecting.commiting}
+
+					<br /><br />Referees correctly protecting player against opponent fouls % =
+					disadv_CC/(disadv_INC+ disadv_CC)
+					<br />{this.state.protecting.disadvantaged}
+
+					<br /><br />Total Correct Protection %:
+					<br />{this.state.protecting.totalPercentage}					
+
+					<br /><br />Comparison to avg protection %:
+
+					<br /><br /> Show everything in bar chart and box chart
+			</div>
+		</div>
+	);
+  }
 }
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+		<RefereeingUI > </RefereeingUI >
+      </div>
+    );
+  }
+}
 export default App;
